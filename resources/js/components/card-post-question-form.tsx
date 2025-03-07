f79abb43faf44bd0c11b8339cc90dc7c2ae59ff0
Elanrif <saidbacoelanrif@gmail.com>
2025-03-07 12:14:15 +0000

    feat: useForm Inertia

--- resources/js/components/card-post-question-form.tsx
+++ resources/js/components/card-post-question-form.tsx
@@ -2,68 +2,131 @@ import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from '@/components/ui/select';
-import { Link } from '@inertiajs/react';
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { Link, useForm } from '@inertiajs/react';
 import { MessageCircleQuestionIcon } from 'lucide-react';
 import { Textarea } from './ui/input-textarea';
 
+type FormData = {
+    theme: string;
+    title: string;
+    content: string;
+};
+const themes: string[] = ['foot', 'manga', 'même', 'poeme', 'art'];
 export function CardPostQuestionForm() {
+    const { data, setData, post, processing, errors, setError, reset } = useForm<FormData>({
+        theme: '',
+        title: '',
+        content: '',
+    });
+
+    function handleChange(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+        const target = e.target;
+        const name = target.name as keyof FormData;
+        const value = target.value;
+        setData(name, value);
+
+        // Effacer l'erreur de validation lorsque l'utilisateur commence à saisir
+        if (errors[name]) {
+            setError(name, ''); // Efface l'erreur pour ce champ
+        }
+    }
+    function handleSelectChange(value: string) {
+        setData('theme', value);
+        // Effacer l'erreur de validation lorsque l'utilisateur sélectionne une valeur
+        if (errors.theme) {
+            setError('theme', '');
+        }
+    }
+
+    function submit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        const errors: Partial<Record<keyof typeof data, string>> = {};
+        if (!data.theme) errors.theme = 'Sélectionner un thème.';
+        if (!data.title) errors.title = 'Veuillez saisir une question.';
+
+        if (Object.keys(errors).length > 0) {
+            (Object.entries(errors) as [keyof typeof data, string][]).forEach(([key, message]) => {
+                setError(key, message);
+            });
+            return;
+        }
+        console.log('data: ', data);
+        //post('/login');
+    }
     return (
-        <Card className="w-[750px]">
-            <div className='flex justify-between items-center'>
-                <CardHeader>
-                    <CardTitle className="flex items-center gap-2">
-                        <MessageCircleQuestionIcon />
-                        Question
-                    </CardTitle>
-                    <CardDescription>Publier une question.</CardDescription>
-                </CardHeader>
-                <CardHeader className="mt-3 hover:text-black duration-300 ease-in-out text-slate-500">
-                    <Link href={route('post.image')}>Postuler une image ?</Link>
-                </CardHeader>
-            </div>
-            <CardContent>
-                <form>
+        <form onSubmit={submit}>
+            <Card className="w-[750px]">
+                <div className="flex items-center justify-between">
+                    <CardHeader>
+                        <CardTitle className="flex items-center gap-2">
+                            <MessageCircleQuestionIcon />
+                            Question
+                        </CardTitle>
+                        <CardDescription>Publier une question.</CardDescription>
+                    </CardHeader>
+                    <CardHeader className="mt-3 text-slate-500 duration-300 ease-in-out hover:text-black">
+                        <Link href={route('post.image')}>Postuler une image ?</Link>
+                    </CardHeader>
+                </div>
+                <CardContent>
                     <div className="grid w-full items-center gap-4">
                         <div>
-                            <Label htmlFor="interest" className="after:ml-1 after:text-red-500 after:content-['*']">
-                                Interest
+                            <Label htmlFor="theme" className="after:ml-1 after:text-red-500 after:content-['*']">
+                                Thème
                             </Label>
-                            <Select>
+                            <Select onValueChange={handleSelectChange} value={data.theme} required>
                                 <SelectTrigger className="w-[280px]" id="interest">
-                                    <SelectValue placeholder="Sélectionner un Interest" />
+                                    <SelectValue id="theme" placeholder="Sélectionner un Interest" />
                                 </SelectTrigger>
                                 <SelectContent>
                                     <SelectGroup>
-                                        <SelectLabel>Interest</SelectLabel>
-                                        <SelectItem value="apple">Foot</SelectItem>
-                                        <SelectItem value="banana">Managa</SelectItem>
-                                        <SelectItem value="blueberry">Même</SelectItem>
-                                        <SelectItem value="grapes">Poème</SelectItem>
-                                        <SelectItem value="pineapple">Art</SelectItem>
+                                        {themes.map((theme, index) => {
+                                            return (
+                                                <SelectItem key={index} value={theme} className="capitalize">
+                                                    {theme}
+                                                </SelectItem>
+                                            );
+                                        })}
                                     </SelectGroup>
                                 </SelectContent>
                             </Select>
+                            {errors.theme && <p className="mt-2 text-sm text-red-500">{errors.theme}</p>}
                         </div>
                         <div className="flex flex-col space-y-1.5">
-                            <Label htmlFor="question" className="after:ml-1 after:text-red-500 after:content-['*']">
+                            <Label htmlFor="title" className="after:ml-1 after:text-red-500 after:content-['*']">
                                 Question?
                             </Label>
-                            <Input id="question" placeholder="Saisir une question" />
+                            <Input id="title" type="text" name="title" value={data.title} onChange={handleChange} placeholder="Saisir une question" />
+                            {errors.title && <p className="mt-2 text-sm text-red-500">{errors.title}</p>}
                         </div>
                         <div className="flex flex-col space-y-1.5">
                             <Label htmlFor="framework" className="after:ml-1 after:text-red-500 after:content-['*']">
                                 Description
                             </Label>
-                            <Textarea id="content" rows={3} placeholder="Text..." aria-rowspan={5} />
+                            <Textarea
+                                id="content"
+                                name="content"
+                                value={data.content}
+                                onChange={handleChange}
+                                rows={5}
+                                placeholder="Contenu..."
+                                required
+                            />
+                            {errors.title && <p className="mt-2 text-sm text-red-500">{errors.content}</p>}
                         </div>
                     </div>
-                </form>
-            </CardContent>
-            <CardFooter className="flex justify-between">
-                <Button variant="outline">Annuler</Button>
-                <Button>Publier</Button>
-            </CardFooter>
-        </Card>
+                </CardContent>
+                <CardFooter className="flex justify-between">
+                    <Button type="reset" variant="outline" onClick={() => reset()}>
+                        Annuler
+                    </Button>
+                    <Button type="submit" disabled={processing}>
+                        Publier
+                    </Button>
+                </CardFooter>
+            </Card>
+        </form>
     );
 }
